e75915a4649de7cfcff7650734906660ab8034e7
ikram-maulana <ikram075@ummi.ac.id>
2022-09-16 17:00:02 +0700

    Feat(Logic): Fix Logic

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ X = df[["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"]]
 y = df["Class"]
 
 # Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Feature Scaling
 sc = StandardScaler()
@@ -20,7 +20,7 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Instantiate model
-classifier = RandomForestClassifier(n_estimators=100, random_state=0)
+classifier = RandomForestClassifier()
 
 # Fit model
 classifier.fit(X_train, y_train)
