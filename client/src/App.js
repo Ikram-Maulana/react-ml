f51d42636e123905fd6015f876d79e9b3e8dd218
ikram-maulana <ikram075@ummi.ac.id>
2022-09-16 11:40:08 +0700

    Feat(UI): Update Front End UI
    - Install chakra-ui
    - Install react-icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,33 @@
-import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Slider,
+  SliderFilledTrack,
+  SliderMark,
+  SliderThumb,
+  SliderTrack,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { MdTipsAndUpdates } from "react-icons/md";
 
 const App = () => {
-  const [title, setTitle] = useState({});
+  const [, setTitle] = useState({});
+  const [sliderSepalLength, setsliderSepalLength] = useState(0.0);
+  const [sliderSepalWidth, setsliderSepalWidth] = useState(0.0);
+  const [sliderPetalLength, setsliderPetalLength] = useState(0.0);
+  const [sliderPetalWidth, setsliderPetalWidth] = useState(0.0);
+  const [showTooltipSepalLength, setShowTooltipSepalLength] = useState(false);
+  const [showTooltipSepalWidth, setShowTooltipSepalWidth] = useState(false);
+  const [showTooltipPetalLength, setShowTooltipPetalLength] = useState(false);
+  const [showTooltipPetalWidth, setShowTooltipPetalWidth] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,13 +39,200 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      {typeof title === "undefined" ? (
-        <p>Loading...</p>
-      ) : (
-        <h1>{title.message}</h1>
-      )}
-    </div>
+    <Container
+      maxW="800px"
+      mt={{ base: "8", md: "12" }}
+      mb={{ base: "8", md: "12" }}
+    >
+      <Flex flexDirection="column" alignItems={{ base: "left", md: "center" }}>
+        <Heading as="h1" size={{ base: "lg", md: "xl" }}>
+          Iris flower species Classification App🌼
+        </Heading>
+        <Text mt="2">ReactJS with Machine Learning</Text>
+      </Flex>
+
+      {/* Form */}
+      <Box mt={{ base: "6", md: "8" }}>
+        <FormControl>
+          <FormLabel>
+            <Flex gap={2}>
+              <Text>Sepal Length (cm):</Text>
+              <Text color={"red"}>{sliderSepalLength} cm</Text>
+            </Flex>
+          </FormLabel>
+          <Box p="6" border="2px" rounded="md">
+            <Slider
+              id="sliderSepalLength"
+              defaultValue={0.0}
+              min={0.0}
+              max={8.0}
+              colorScheme="red"
+              step={0.01}
+              onChange={(v) => setsliderSepalLength(v)}
+              onMouseEnter={() => setShowTooltipSepalLength(true)}
+              onMouseLeave={() => setShowTooltipSepalLength(false)}
+            >
+              <SliderMark value={0} mt="1" ml="-2.5" fontSize="sm">
+                0.00
+              </SliderMark>
+              <SliderMark value={8} mt="1" ml="-2.5" fontSize="sm">
+                8.00
+              </SliderMark>
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <Tooltip
+                hasArrow
+                bg="red.500"
+                color="white"
+                placement="top"
+                isOpen={showTooltipSepalLength}
+                label={`${sliderSepalLength} cm`}
+              >
+                <SliderThumb />
+              </Tooltip>
+            </Slider>
+          </Box>
+
+          <FormLabel mt="2">
+            <Flex gap={2}>
+              <Text>Sepal Width (cm):</Text>
+              <Text color={"red"}>{sliderSepalWidth} cm</Text>
+            </Flex>
+          </FormLabel>
+          <Box p="6" border="2px" rounded="md">
+            <Slider
+              id="slider"
+              defaultValue={0.0}
+              min={0.0}
+              max={8.0}
+              colorScheme="red"
+              step={0.01}
+              onChange={(v) => setsliderSepalWidth(v)}
+              onMouseEnter={() => setShowTooltipSepalWidth(true)}
+              onMouseLeave={() => setShowTooltipSepalWidth(false)}
+            >
+              <SliderMark value={0} mt="1" ml="-2.5" fontSize="sm">
+                0.00
+              </SliderMark>
+              <SliderMark value={8} mt="1" ml="-2.5" fontSize="sm">
+                8.00
+              </SliderMark>
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <Tooltip
+                hasArrow
+                bg="red.500"
+                color="white"
+                placement="top"
+                isOpen={showTooltipSepalWidth}
+                label={`${sliderSepalWidth} cm`}
+              >
+                <SliderThumb />
+              </Tooltip>
+            </Slider>
+          </Box>
+
+          <FormLabel mt="2">
+            <Flex gap={2}>
+              <Text>Petal Length (cm):</Text>
+              <Text color={"red"}>{sliderPetalLength} cm</Text>
+            </Flex>
+          </FormLabel>
+          <Box p="6" border="2px" rounded="md">
+            <Slider
+              id="slider"
+              defaultValue={0.0}
+              min={0.0}
+              max={8.0}
+              colorScheme="red"
+              step={0.01}
+              onChange={(v) => setsliderPetalLength(v)}
+              onMouseEnter={() => setShowTooltipPetalLength(true)}
+              onMouseLeave={() => setShowTooltipPetalLength(false)}
+            >
+              <SliderMark value={0} mt="1" ml="-2.5" fontSize="sm">
+                0.00
+              </SliderMark>
+              <SliderMark value={8} mt="1" ml="-2.5" fontSize="sm">
+                8.00
+              </SliderMark>
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <Tooltip
+                hasArrow
+                bg="red.500"
+                color="white"
+                placement="top"
+                isOpen={showTooltipPetalLength}
+                label={`${sliderPetalLength} cm`}
+              >
+                <SliderThumb />
+              </Tooltip>
+            </Slider>
+          </Box>
+
+          <FormLabel mt="2">
+            <Flex gap={2}>
+              <Text>Petal Width (cm):</Text>
+              <Text color={"red"}>{sliderPetalWidth} cm</Text>
+            </Flex>
+          </FormLabel>
+          <Box p="6" border="2px" rounded="md">
+            <Slider
+              id="slider"
+              defaultValue={0.0}
+              min={0.0}
+              max={8.0}
+              colorScheme="red"
+              step={0.01}
+              onChange={(v) => setsliderPetalWidth(v)}
+              onMouseEnter={() => setShowTooltipPetalWidth(true)}
+              onMouseLeave={() => setShowTooltipPetalWidth(false)}
+            >
+              <SliderMark value={0} mt="1" ml="-2.5" fontSize="sm">
+                0.00
+              </SliderMark>
+              <SliderMark value={8} mt="1" ml="-2.5" fontSize="sm">
+                8.00
+              </SliderMark>
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <Tooltip
+                hasArrow
+                bg="red.500"
+                color="white"
+                placement="top"
+                isOpen={showTooltipPetalWidth}
+                label={`${sliderPetalWidth} cm`}
+              >
+                <SliderThumb />
+              </Tooltip>
+            </Slider>
+          </Box>
+
+          <Button
+            leftIcon={<MdTipsAndUpdates />}
+            colorScheme="red"
+            variant="solid"
+            mt="3"
+          >
+            Click Here to Classify
+          </Button>
+        </FormControl>
+      </Box>
+
+      <Heading as="h2" mt="8">
+        Result:{" "}
+      </Heading>
+      <Box p="6" mt="2" bg={"gray.500"} color="white" rounded="md">
+        <Text>The Flower Species is:</Text>
+        <Heading>No Result</Heading>
+      </Box>
+    </Container>
   );
 };
 
